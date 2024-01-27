993eabe8c176511a4efed851494377b2cdd389c4
강지원 <kangji149@naver.com>
2024-01-28 05:57:45 +0900

    feat : add global controller advice

---
+++ src/main/java/com/bonjung/camong/common/exception/GlobalControllerAdvice.java
@@ -0,0 +1,34 @@
+package com.bonjung.camong.common.exception;
+
+import com.bonjung.camong.common.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(value = CustomException.class)
+    public ResponseEntity<?> errorHandler(CustomException e) {
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+                .body(Response.error(e.getErrorCode(), e.getMessage()));
+    }
+
+//    @ExceptionHandler(value = InsufficientAuthenticationException.class)
+//    public ResponseEntity<?> errorHandler(Exception e) {
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+//                .body(Response.error(ErrorCode.UNAUTHORIZED_USER, e.getMessage()));
+//    }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<?> unknownErrorHandler(Exception e) {
+        log.error("CustomContollerAdvice", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage()));
+    }
+}
+
+}
