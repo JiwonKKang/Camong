ea3effa23d0277de16e8b25429d919be03385f83
강지원 <kangji149@naver.com>
2024-04-12 09:00:00 +0000

    feat: swagger https setting

--- src/main/java/com/bonjung/camong/CamongApplication.java
+++ src/main/java/com/bonjung/camong/CamongApplication.java
@@ -1,11 +1,14 @@
 package com.bonjung.camong;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "https://camong.p-e.kr")})
 public class CamongApplication {
 
 	public static void main(String[] args) {
